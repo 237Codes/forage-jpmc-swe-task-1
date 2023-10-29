68f758af1948126ab77b500102b226a1291adf02
Manyaka <104553245+237Codes@users.noreply.github.com>
2023-10-29 06:10:13 -0400

    Create changes required by instructions

--- client_test.py
+++ client_test.py
@@ -1,5 +1,5 @@
 import unittest
-from client3 import getDataPoint
+from client3 import getDataPoint, getRatio
 
 class ClientTest(unittest.TestCase):
   def test_getDataPoint_calculatePrice(self):
@@ -8,6 +8,8 @@ class ClientTest(unittest.TestCase):
       {'top_ask': {'price': 121.68, 'size': 4}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 117.87, 'size': 81}, 'id': '0.109974697771', 'stock': 'DEF'}
     ]
     """ ------------ Add the assertion below ------------ """
+    for quote in quotes: # Add assertion for the expected result
+       self.assertEqual(getDataPoint(quote), (quote['stock'], quote['top_bid']['price'], quote['top_ask']['price'], (quote['top_bid']['price'] + quote['top_ask']['price']) / 2))
 
   def test_getDataPoint_calculatePriceBidGreaterThanAsk(self):
     quotes = [
@@ -15,9 +17,25 @@ class ClientTest(unittest.TestCase):
       {'top_ask': {'price': 121.68, 'size': 4}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 117.87, 'size': 81}, 'id': '0.109974697771', 'stock': 'DEF'}
     ]
     """ ------------ Add the assertion below ------------ """
+    for quote in quotes: # Add assertion for the expected result
+       self.assertEqual(getDataPoint(quote), (quote['stock'], quote['top_bid']['price'], quote['top_ask']['price'], (quote['top_bid']['price'] + quote['top_ask']['price']) / 2))
 
+  def test_getRatio(self):
+    # Test with various inputs for getRatio() function
+    result_1 = getRatio(10,5)  #both positivee case
+    self.assertEqual(result_1, 2)  #check if result matches expected values
 
-  """ ------------ Add more unit tests ------------ """
+    result_2 = getRatio(10,0)  #zero denominator case
+    self.assertEqual(result_2, 0)
+
+    result_3 = getRatio(0,10) #zero numerator case
+    self.assertEqual(result_3, 0)
+
+    result_4 = getRatio(0,0) #both zero case
+    self.assertEqual(result_4, 0)
+
+    result_5 = getRatio(-10,5) #negative number case
+    self.assertEqual(result_5, -2)
 
 
 
